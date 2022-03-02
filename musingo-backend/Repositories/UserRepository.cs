142c6c57ddb8d759e6420bb241d92f499f08d985
rtrydev <r.kulka44@gmail.com>
2022-03-02 17:26:19 +0100

    temporary user repository

--- musingo-backend/Repositories/UserRepository.cs
+++ musingo-backend/Repositories/UserRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using musingo_backend.Data;
 using musingo_backend.Models;
 
@@ -5,12 +6,32 @@ namespace musingo_backend.Repositories;
 
 public interface IUserRepository
 {
-    public Task<ICollection<User>> GetAll();
+    public Task<User?> GetUserById(int id);
+    public Task<User?> LoginUser(string login, string password);
+    public Task<User?> AddUser(User user);
+    
 }
 
-public class UserRepository : Repository<User>
+public class UserRepository : Repository<User>, IUserRepository
 {
     public UserRepository(RepositoryContext context) : base(context) { }
-    
-    
+
+
+    public async Task<User?> GetUserById(int id)
+    {
+        var result = await GetAll().FirstOrDefaultAsync(x => x.Id == id);
+        return result;
+    }
+
+    public async Task<User?> LoginUser(string login, string password)
+    {
+        var result = await GetAll().FirstOrDefaultAsync(x => x.Email == login && x.Password == password);
+        return result;
+    }
+
+    public async Task<User?> AddUser(User user)
+    {
+        var result = await AddAsync(user);
+        return result;
+    }
 }
