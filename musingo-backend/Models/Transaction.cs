e5f7689070f33e35fe567b7b2c3fdcbd003d3b7c
rtrydev <r.kulka44@gmail.com>
2022-03-02 13:29:45 +0100

    add transaction model

--- musingo-backend/Models/Transaction.cs
+++ musingo-backend/Models/Transaction.cs
@@ -1,6 +1,18 @@
 namespace musingo_backend.Models;
 
+public enum TransactionStatus
+{
+    Opened,
+    UnderNegotiation,
+    Finished,
+    Declined
+}
 public class Transaction
 {
-    
+    public int Id { get; set; }
+    public Offer? Offer { get; set; }
+    public User? Seller { get; set; }
+    public User? Buyer { get; set; }
+    public DateTime LastUpdateTime { get; set; }
+    public TransactionStatus Status { get; set; }
 }
