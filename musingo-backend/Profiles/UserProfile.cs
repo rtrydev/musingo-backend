10fae485ecf892b736daf127213f2e6a4928b841
rtrydev <r.kulka44@gmail.com>
2022-03-02 17:37:40 +0100

    add user profile

---
+++ musingo-backend/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+using AutoMapper;
+using musingo_backend.Dtos;
+using musingo_backend.Models;
+
+namespace musingo_backend.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<User, UserDto>();
+        CreateMap<UserDto, User>();
+    }
+}
