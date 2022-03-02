16f4614393ef880f3b60c3db19501b1fd26859c9
rtrydev <r.kulka44@gmail.com>
2022-03-02 13:32:55 +0100

    add user model

--- musingo-backend/Models/User.cs
+++ musingo-backend/Models/User.cs
@@ -2,5 +2,9 @@ namespace musingo_backend.Models;
 
 public class User
 {
-    
+    public int Id { get; set; }
+    public string? Email { get; set; }
+    public string? Name { get; set; }
+    public string? Password { get; set; }
+    public string? ImageUrl { get; set; }
 }
